cccb1262a4ecbc1b9b3a60b7f6c2e7d148e0bd51
williamTess <william.tessari@epitech.eu>
2024-01-22 14:21:13 +0100

    create routes + empty pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProfilePage from "./pages/Profile";
+import SignInPage from "./pages/SignIn";
+import SignUpPage from "./pages/SignUp";
+import AboutPage from "./pages/About";
 
 export default function App() {
-  return <div className="text-red-800">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Router>
+  );
 }
