d91b184d28ae60d8c9b32af3f444d213e47901e1
williamTess <william.tessari@epitech.eu>
2024-01-23 18:32:13 +0100

    create sign in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +12,26 @@ export const signup = async (req, res, next) => {
     res
       .status(201)
       .json({ success: true, message: "User created successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) next(errorHandler(401, "Invalid credentials"));
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("acces_token", token, { httpOnly: true })
+      .status(200)
+      .json({ ...rest, success: true });
   } catch (err) {
     next(err);
   }
