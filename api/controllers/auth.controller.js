b78cdc3d641404fbb70969fc0aed13577e311749
williamTess <william.tessari@epitech.eu>
2024-01-30 15:55:28 +0100

    fix authent sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,12 +21,12 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) next(errorHandler(404, "User not found"));
+    if (!validUser) return next(errorHandler(404, "User not found"));
 
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     console.log(validPassword, validUser);
-    if (!validPassword) next(errorHandler(401, "Invalid credentials"));
-    console.log("2222222", validPassword, validUser);
+    if (!!!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
     const { password: hashedPassword, ...rest } = validUser._doc;
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
