598b42692b1a27c51a76c062eb88ff9c8d209977
williamTess <william.tessari@epitech.eu>
2024-02-07 11:54:58 +0100

    Add footer

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,3 +1,82 @@
+import { ReactElement } from "react";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { ButtonMUI } from "../ButtonMUI";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 export const Footer = () => {
-  return <div className="bg-slate-800 text-white py-5 px-10">test</div>;
+  const Column = ({
+    title,
+    children,
+  }: {
+    title: string;
+    children?: ReactElement;
+  }) => {
+    return (
+      <div className="flex flex-col max-w-56">
+        <h3 className="text-blue-500 font-semibold mb-3 text-lg">{title}</h3>
+        {children}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="bg-slate-800 text-white p-10">
+        <p className="mb-10">Test</p>
+        <div className="flex flex-row justify-between mb-10">
+          <Column title="A propos">
+            <>
+              <p className="mb-3">
+                NouProprio, une entreprise qui partage des informations
+                immobilières pour répondre aux non-dits du domaine
+              </p>
+              <ButtonMUI className="max-w-24" value={"Accueil"} />
+            </>
+          </Column>
+          <Column title="Notre SAV">
+            <>
+              <p className="mb-3">Bientôt disponible</p>
+              <a
+                className="text-blue-600 font-semibold hover:underline"
+                href="mailto:will94290@yahoo.fr"
+              >
+                <ArrowForwardIcon fontSize="small" /> Nous contacter
+              </a>
+            </>
+          </Column>
+          <Column title="Suivez nous sur les réseaux sociaux">
+            <>
+              <p className="mb-1">Bientôt disponible: </p>
+              <p className="mb-1">
+                <InstagramIcon /> Instagram
+              </p>
+              <p className="mb-1">
+                <TwitterIcon /> Twitter
+              </p>
+              <p className="mb-5">
+                <LinkedInIcon /> Linkedin
+              </p>
+              <div className="flex gap-2 max-w-10">
+                <ButtonMUI value={<InstagramIcon />} />
+                <ButtonMUI value={<TwitterIcon />} />
+                <ButtonMUI value={<LinkedInIcon />} />
+              </div>
+            </>
+          </Column>
+          <Column title="Contactez nous">
+            <>
+              <p className="mb-3">Bientôt disponible</p>
+              <ButtonMUI className="max-w-24" value={"Contact"} />
+            </>
+          </Column>
+        </div>
+        <p>NousProprios, notre entreprise</p>
+      </div>
+      <div className="bg-slate-900 text-white px-10 py-5">
+        NousProprios . CGV . Mentions légales . Politique de confidentialité
+      </div>
+    </>
+  );
 };
