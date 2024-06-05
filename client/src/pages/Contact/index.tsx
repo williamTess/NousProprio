598da1875dc5dac5e4b6249746e16da9577e32c9
William Tessari <william.tessari@epitech.eu>
2024-06-05 15:37:49 +0200

    correct typo

--- client/src/pages/Contact/index.tsx
+++ client/src/pages/Contact/index.tsx
@@ -4,7 +4,7 @@ export const ContactPage = () => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-col py-10 px-10 sm:px-20">
-        <h1 className="text-center text-3xl font-bold p-5">Nos Valeurs</h1>
+        <h1 className="text-center text-3xl font-bold p-5">Notre FAQ</h1>
         <FAQ />
       </div>
     </div>
