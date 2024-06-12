4d123e5b87986818622e13c79e130d76245008ae
William Tessari <william.tessari@epitech.eu>
2024-06-12 21:45:22 +0200

    added google + corrector

--- client/src/pages/Home/Comments.tsx
+++ client/src/pages/Home/Comments.tsx
@@ -1,109 +1,16 @@
-import React from "react";
-import { commentaries } from "../../constant";
-
-import Box from "@mui/material/Box";
-import { useTheme } from "@mui/material/styles";
-import MobileStepper from "@mui/material/MobileStepper";
-import Button from "@mui/material/Button";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import { Comment } from "../../components/Comment";
-
 type CommentsProps = {
   sizeAccordingWidth: string;
   isPortrait: boolean;
 };
 
-export const Comments = ({ sizeAccordingWidth, isPortrait }: CommentsProps) => {
-  const theme = useTheme();
-  const commentsPerSlide = isPortrait ? 1 : 3;
-  const [activeStep, setActiveStep] = React.useState(
-    commentsPerSlide > 1 ? 1 : 0
-  );
-  const maxSteps = commentaries.length - (commentsPerSlide - 1);
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+export const Comments = ({ sizeAccordingWidth }: CommentsProps) => {
   return (
     <div className={`${sizeAccordingWidth} flex flex-col max-w-7xl m-auto`}>
-      <div className="flex flex-col md:flex-row md:justify-between mb-4 md:mb-0">
-        <h1 className="font-semibold text-3xl mb-2 md:mb-10">
-          L'avis des étudiants
-        </h1>
-        <a
-          href="https://fr.trustpilot.com/review/nous-proprio.onrender.com"
-          target="_blank"
-        >
-          <p className="text-blue-400 underline">Voir les commentaires</p>
-        </a>
-      </div>
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: "100%",
-          flexGrow: 1,
-          alignSelf: "center",
-        }}
-      >
-        <div className="flex justify-center md:justify-between">
-          {commentaries[activeStep - 1] && (
-            <Comment
-              comment={commentaries[activeStep - 1]}
-              className={`${isPortrait && "hidden"}`}
-            />
-          )}
-          <Comment comment={commentaries[activeStep]} />
-          {commentaries[activeStep + 1] && (
-            <Comment
-              comment={commentaries[activeStep + 1]}
-              className={`${isPortrait && "hidden"}`}
-            />
-          )}
-        </div>
-        <MobileStepper
-          variant="text"
-          steps={maxSteps}
-          position="static"
-          className="!bg-gray-light !text-white"
-          activeStep={commentsPerSlide > 1 ? activeStep - 1 : activeStep}
-          nextButton={
-            <Button
-              size="small"
-              onClick={handleNext}
-              disabled={
-                activeStep ===
-                (commentsPerSlide === 1 ? maxSteps - 1 : maxSteps)
-              }
-            >
-              Suivant
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowLeft />
-              ) : (
-                <KeyboardArrowRight />
-              )}
-            </Button>
-          }
-          backButton={
-            <Button
-              size="small"
-              onClick={handleBack}
-              disabled={activeStep === (commentsPerSlide === 1 ? 0 : 1)}
-            >
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowRight />
-              ) : (
-                <KeyboardArrowLeft />
-              )}
-              Précédent
-            </Button>
-          }
-        />
-      </Box>
+      <h1 className="font-semibold text-3xl mb-10">L'avis des étudiants</h1>
+      <div
+        className="elfsight-app-765c2ea1-ef81-480f-9183-2d648a567d2a"
+        data-elfsight-app-lazy
+      ></div>
     </div>
   );
 };
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -17,7 +17,7 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col h-[250px] sm:h-[500px] overflow-x-hidden overflow-y-hidden -mt-24 sm:-mt-52">
+      <div className="flex flex-col h-[250px] sm:h-[600px] 2xl:h-[800px] overflow-x-hidden overflow-y-hidden -mt-24 sm:-mt-52">
         <video
           src={VideoHeader}
           loop
@@ -26,23 +26,23 @@ const HomePage = () => {
           className="object-cover object-center h-screen w-screen -z-10"
         ></video>
       </div>
-      <div className="bg-gray-light text-white flex flex-col py-10 sm:pt-10 pt-5 px-10 sm:px-20">
+      <div className="bg-gray-light text-white flex flex-col py-8 pt-5 px-10 sm:px-20">
         <Presentation
           videoRef={videoRef}
           sizeAccordingWidth={sizeAccordingWidth}
           isPortrait={isPortrait}
         />
       </div>
-      <div className="py-10 px-10 sm:px-20 flex flex-col" ref={videoRef}>
+      <div className="py-8 px-10 sm:px-20 flex flex-col" ref={videoRef}>
         <Boss sizeAccordingWidth={sizeAccordingWidth} isPortrait={isPortrait} />
       </div>
-      <div className="py-10 px-10 sm:px-20 flex flex-col bg-gray-light text-white">
+      <div className="py-8 px-10 sm:px-20 flex flex-col bg-gray-light text-white">
         <Comments
           sizeAccordingWidth={sizeAccordingWidth}
           isPortrait={isPortrait}
         />
       </div>
-      <div className="py-10 px-10 sm:px-20 flex flex-col">
+      <div className="py-8 px-10 sm:px-20 flex flex-col">
         <Video
           sizeAccordingHeight={sizeAccordingHeight}
           sizeAccordingWidth={sizeAccordingWidth}
